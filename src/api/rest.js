b88462cf47c68ae447dfa6b8007aeaca9f79729d
JLJL <juanluisjaramillolopez@gmail.com>
2024-02-15 20:25:48 -0600

    creacion del formulario para la captura de datos, ademas del uso de react-hook-form para la evalucion de los datos ademas del llamado de l api con react-query

--- src/api/rest.js
+++ src/api/rest.js
@@ -1,1 +1,17 @@
 //en este archivo js se crearan las peticiones de un CRUD basico
+import axios from "axios";
+//creamos la base url de nuestro backend
+
+const baseUrl = axios.create({
+  baseURL: "http://localhost:3000/tasks",
+  timeout: 2000,
+  timeoutErrorMessage: "Timeout error"
+});
+
+export const getTask = async () => {
+  const result = await baseUrl.get("/");
+  return result.data; //result.data trae solo la data del json
+};
+export const postTask = (task) => baseUrl.post("/", task);
+export const deleteTask = (id) => baseUrl.get(`/${id}`);
+export const putTask = (product) => baseUrl.get(`/${product.id}`, product);
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,8 +1,31 @@
 //este componente tendra la lista de todas las tareas que existen en le json
-const TaskList = () => {
-  return (
-    <div>TaskList</div>
-  )
-}
+import { useQuery } from "@tanstack/react-query";
+import { getTask } from "../api/rest";
 
-export default TaskList
+const TaskList = () => {
+  //llamos la data que trae la peticion
+
+  const { isLoading, isError, error, data } = useQuery({
+    queryKey: ["task"],
+    queryFn: getTask,
+  });
+  if (isLoading)
+    return (
+      <div className="spinner- text-danger" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    );
+  if (isError) return <h1>{error}</h1>;
+
+  return (
+    <div>
+      {data.map((task) => (
+        <div key={task.id}>
+          <p>{task.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default TaskList;
